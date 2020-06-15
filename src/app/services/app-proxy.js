b9bfc1811267998583022fc9606a10f2e14e23cb
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-06-15 15:17:40 +0200

    VFS-6421 Added appProxy service

---
+++ src/app/services/app-proxy.js
@@ -0,0 +1,84 @@
+/**
+ * Exposes Onezone application proxy API (available through window.frameElement.appProxy).
+ *
+ * @module services/app-proxy
+ * @author Michał Borzęcki
+ * @copyright (C) 2020 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import Service from '@ember/service';
+import { get } from '@ember/object';
+import { Promise, resolve } from 'rsvp';
+import { later } from '@ember/runloop';
+
+export default class AppProxyService extends Service {
+  /**
+   * @type {Object}
+   */
+  appProxy = null;
+
+  /**
+   * @type {Promise}
+   */
+  appProxyLoadingPromise = resolve();
+
+  /**
+   * Elasticsearch request function
+   * @type {Function}
+   * @param {String} params.method one of `get`, `post`
+   * @param {String} params.indexName
+   * @param {String} params.path path to resource (part of the url without index)
+   * @param {String|undefined} params.body request body
+   * @returns {Promise<any>} request result
+   */
+  get dataRequest() {
+    return (this.appProxy || {}).dataRequest;
+  }
+
+  /**
+   * Fetches injected GUI plugin configuration
+   * @type {Function}
+   * @returns {Promise<Object>}
+   */
+  get configRequest() {
+    return (this.appProxy || {}).configRequest;
+  }
+
+  constructor() {
+    super(...arguments);
+
+    if (!this.loadAppProxy()) {
+      this.appProxyLoadingPromise = this.scheduleLoadAppProxy();
+    }
+  }
+
+  /**
+   * @returns {Window}
+   */
+  getWindow() {
+    return window;
+  }
+
+  /**
+   * @returns {Object}
+   */
+  loadAppProxy() {
+    return this.appProxy = get(this.getWindow(), 'frameElement.appProxy') || null;
+  }
+
+  /**
+   * @returns {Promise}
+   */
+  scheduleLoadAppProxy() {
+    return new Promise(resolve => {
+      later(this, () => {
+        if (!this.loadAppProxy()) {
+          this.scheduleLoadAppProxy().then(resolve);
+        } else {
+          resolve();
+        }
+      }, 10);
+    });
+  }
+}
---
+++ src/tests/unit/services/app-proxy-test.js
@@ -0,0 +1,89 @@
+import { expect } from 'chai';
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { setupTest } from 'ember-mocha';
+import sinon from 'sinon';
+import AppProxy from 'harvester-gui-plugin-generic/services/app-proxy';
+
+describe('Unit | Service | app-proxy', function () {
+  setupTest();
+
+  beforeEach(function () {
+    const appProxy = {
+      dataRequest: () => {},
+      configRequest: () => {},
+    };
+    const windowMock = {
+      frameElement: {
+        appProxy,
+      },
+    };
+    sinon.stub(AppProxy.prototype, 'getWindow').returns(windowMock);
+    this.setProperties({
+      appProxy,
+      windowMock,
+    });
+  });
+
+  afterEach(function () {
+    const fakeClock = this.fakeClock;
+    if (fakeClock) {
+      fakeClock.restore();
+    }
+    if (AppProxy.prototype.getWindow.restore) {
+      AppProxy.prototype.getWindow.restore();
+    }
+  });
+
+  it('returns window global object on getWindow() call', function () {
+    const service = this.owner.lookup('service:app-proxy');
+    service.getWindow.restore();
+
+    expect(service.getWindow()).to.equal(window);
+  });
+
+  it('loads parent application appProxy on init', function () {
+    const service = this.owner.lookup('service:app-proxy');
+    expect(service.appProxy).to.equal(this.get('appProxy'));
+  });
+
+  [
+    'dataRequest',
+    'configRequest',
+  ].forEach(injectedPropName => {
+    it(
+      `loads ${injectedPropName} from appProxy to the ${injectedPropName} field`,
+      function () {
+        const service = this.owner.lookup('service:app-proxy');
+        expect(service[injectedPropName])
+          .to.equal(this.get(`appProxy.${injectedPropName}`));
+      }
+    );
+  });
+
+  it(
+    'tries to load appProxy continuously when it is not available on init',
+    function () {
+      this.fakeClock = sinon.useFakeTimers();
+      const {
+        windowMock,
+        appProxy,
+      } = this.getProperties('windowMock', 'appProxy');
+      windowMock.frameElement = {};
+      const promiseResolveSpy = sinon.spy();
+
+      const service = this.owner.lookup('service:app-proxy');
+      service.appProxyLoadingPromise.then(promiseResolveSpy);
+      expect(service.appProxy).to.be.null;
+      expect(promiseResolveSpy).to.be.not.called;
+
+      this.fakeClock.tick(15);
+      expect(service.appProxy).to.be.null;
+      expect(promiseResolveSpy).to.be.not.called;
+
+      windowMock.frameElement.appProxy = appProxy;
+      this.fakeClock.tick(15);
+      expect(service.appProxy).to.equal(appProxy);
+      expect(promiseResolveSpy).to.be.calledOnce;
+    }
+  );
+});
