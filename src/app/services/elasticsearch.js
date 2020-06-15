37bfb04ea56ed1346865266eaed8fc71de56a2e7
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-06-15 15:49:43 +0200

    VFS-6421 added elasticsearch service

---
+++ src/app/services/elasticsearch.js
@@ -0,0 +1,54 @@
+/**
+ * Exposes REST methods of Elasticsearch
+ *
+ * @module services/elasticsearch
+ * @author Michał Borzęcki
+ * @copyright (C) 2020 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import Service, { inject as service } from '@ember/service';
+
+export default class ElasticsearchService extends Service {
+  @service appProxy;
+
+  /**
+   * Performs request to Elasticsearch.
+   * @param {String} method one of `get`, `post`
+   * @param {String} indexName
+   * @param {String} path url (without host and index)
+   * @param {any} body request body
+   * @returns {Promise<any>} request result
+   */
+  request(method, indexName, path, body) {
+    const dataRequest = this.get('appProxy.dataRequest');
+    return dataRequest({
+      method,
+      indexName,
+      path,
+      body: JSON.stringify(body),
+    });
+  }
+
+  /**
+   * Makes a GET request
+   * `fetch` because `get` is an Ember reserved function name
+   * @param {String} indexName
+   * @param {String} path
+   * @returns {Promise<any>}
+   */
+  fetch(indexName, path) {
+    return this.request('get', indexName, path);
+  }
+
+  /**
+   * Makes a POST request
+   * @param {String} indexName
+   * @param {String} path
+   * @param {any} body
+   * @returns {Promise<any>}
+   */
+  post(indexName, path, body) {
+    return this.request('post', indexName, path, body);
+  }
+}
---
+++ src/tests/unit/services/elasticsearch-test.js
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { setupTest } from 'ember-mocha';
+import sinon from 'sinon';
+import AppProxy from 'harvester-gui-plugin-generic/services/app-proxy';
+
+describe('Unit | Service | elasticsearch', function () {
+  setupTest();
+
+  beforeEach(function () {
+    sinon.stub(AppProxy.prototype, 'loadAppProxy').returns({});
+
+    const dataRequestStub = sinon.stub().resolves('result');
+    sinon.stub(this.owner.lookup('service:app-proxy'), 'dataRequest')
+      .get(() => dataRequestStub);
+
+    this.set('dataRequestStub', dataRequestStub);
+  });
+
+  afterEach(function () {
+    if (AppProxy.prototype.loadAppProxy.restore) {
+      AppProxy.prototype.loadAppProxy.restore();
+    }
+  });
+
+  it('performs get request on fetch() call', function () {
+    const service = this.owner.lookup('service:elasticsearch');
+    const dataRequestStub = this.get('dataRequestStub');
+
+    return service.fetch('someIndex', '_test')
+      .then(result => {
+        expect(result).to.equal('result');
+        expect(dataRequestStub).to.be.calledWith(sinon.match({
+          method: 'get',
+          indexName: 'someIndex',
+          path: '_test',
+          body: undefined,
+        }));
+        expect(dataRequestStub).to.be.calledOnce;
+      });
+  });
+
+  it('performs post request on post() call', function () {
+    const service = this.owner.lookup('service:elasticsearch');
+    const dataRequestStub = this.get('dataRequestStub');
+
+    return service.post('someIndex', '_test', { a: 'b' })
+      .then(result => {
+        expect(result).to.equal('result');
+        expect(dataRequestStub).to.be.calledWith(sinon.match({
+          method: 'post',
+          indexName: 'someIndex',
+          path: '_test',
+          body: '{"a":"b"}',
+        }));
+        expect(dataRequestStub).to.be.calledOnce;
+      });
+  });
+});
